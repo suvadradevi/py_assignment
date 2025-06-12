89884a4bb61e87ca8fc6d4923c6500a0bf396a49
suvadradevi <suvadradevipanigrahy@gmail.com>
2025-06-13 00:21:33 +0530

    " added task1.py"

---
+++ task1.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter first number :"))
+n2=int(input("Enter second number :"))
+a=n1+n2
+b=n1-n2
+c=n1*n2
+d=n1/n2
+print("Addition :",a)
+print("Subtraction :",b)
+print("Multiplication :",c)
+print("Division :",d)
